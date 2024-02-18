64ee0a39dfa39c7f0d6b0db9ecad31af6e0bb635
Al-nizar Wahab <108575639+Zhar-dev@users.noreply.github.com>
2024-02-18 23:38:21 +0800

    Added Files

---
+++ conn.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "user";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Successfully Connected to the Server";
+
+$conn->close();
+?>
